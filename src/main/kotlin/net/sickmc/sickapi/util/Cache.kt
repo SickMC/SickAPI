c4ce92bb33c076eb5181feaacd1ab5d3721a376b
Anton <anton.wiblishauser@gmx.de>
2022-09-17 13:37:42 +0200

    protect cache

--- src/main/kotlin/net/sickmc/sickapi/util/Cache.kt
+++ src/main/kotlin/net/sickmc/sickapi/util/Cache.kt
@@ -4,9 +4,8 @@ import kotlinx.coroutines.sync.Mutex
 
 abstract class Cache<T, V> {
 
-    abstract val mutex: Mutex
-
-    abstract val cache: HashMap<T, V>
+    protected abstract val mutex: Mutex
+    protected abstract val cache: HashMap<T, V>
 
     abstract suspend fun get(key: T): V?
     abstract suspend fun set(key: T, value: V, create: Boolean = false): V

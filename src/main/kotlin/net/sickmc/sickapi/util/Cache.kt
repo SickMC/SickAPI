7e15ce7bfba89427c105fa88a2f5ab6504e9076b
Anton <anton.wiblishauser@gmx.de>
2022-09-26 17:44:53 +0200

    improve cache

--- src/main/kotlin/net/sickmc/sickapi/util/Cache.kt
+++ src/main/kotlin/net/sickmc/sickapi/util/Cache.kt
@@ -1,12 +1,55 @@
 package net.sickmc.sickapi.util
 
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-abstract class Cache<T, V> {
+class Cache<T, V>(private val loader: suspend T.() -> V?, private val creator: suspend T.() -> V) {
 
-    protected abstract val mutex: Mutex
-    protected abstract val cache: HashMap<T, V>
+    val mutex: Mutex = Mutex()
+    val cache: HashMap<T, V> = hashMapOf()
 
-    abstract suspend fun get(key: T): V?
-    abstract suspend fun set(key: T, value: V, create: Boolean = false): V
+    suspend fun get(key: T, reload: Boolean = false): V? {
+        if (reload) {
+            val new = loader.invoke(key) ?: return null
+            cache[key] = new
+            return new
+        }
+        mutex.withLock {
+            return cache[key] ?: kotlin.run {
+                val new = loader.invoke(key) ?: return null
+                cache[key] = new
+                return new
+            }
+        }
+    }
+
+    suspend fun create(key: T): V {
+        val created = creator(key)
+        cache[key] = created
+        return created
+    }
+}
+
+inline fun <reified T, reified V> cache(crossinline builder: CacheBuilder<T, V>.() -> Unit): Cache<T, V> {
+    return CacheBuilder<T, V>().apply(builder).build()
+}
+
+class CacheBuilder<T, V> {
+
+    var loader: (suspend T.() -> V?)? = null
+    var creator: (suspend T.() -> V)? = null
+
+    fun loadItem(loader: suspend T.() -> V?) {
+        this.loader = loader
+    }
+
+    fun createItem(creator: suspend T.() -> V) {
+        this.creator = creator
+    }
+
+    fun build(): Cache<T, V> {
+        if (loader == null || creator == null) error("Loader or creator in cache was null!")
+        return Cache(loader!!, creator!!)
+    }
+
 }

6bfd40a857eb51a1cb512c5ae258b02e7d087226
Anton <anton.wiblishauser@gmx.de>
2022-09-27 19:35:09 +0200

    add colors

--- src/main/kotlin/net/sickmc/sickapi/util/Color.kt
+++ src/main/kotlin/net/sickmc/sickapi/util/Color.kt
@@ -23,3 +23,23 @@ fun Gradient.getColor(percent: Double): Int {
     )
     return color.red * 65536 + color.green * 256 + color.blue
 }
+
+
+object Colors {
+    val black = 0x000000
+    val white = 0xFFFFFF
+    val gray = 0x585557
+    val red = 0xC40B0D
+    val darkRed = 0x5A0607
+    val green = 0x00DD04
+    val darkGreen = 0x005703
+    val yellow = 0xEFDC08
+    val darkYellow = 0x9C9005
+    val lightBlue = 0x66B7FF
+    val blue = 0x5D77FF
+    val darkBlue = 0x2E3BFF
+    val pink = 0xFF58DF
+    val purple = 0x751B6B
+    val brown = 0x693400
+    val gold = 0xD09109
+}

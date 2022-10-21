b7ef8538cfaee387f61bfc08c63b093d60b7e423
Anton <anton.wiblishauser@gmx.de>
2022-10-21 16:19:42 +0200

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 @file:Suppress("SpellCheckingInspection")
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
 
     id("com.github.breadmoirai.github-release") version "2.+"
     `maven-publish`

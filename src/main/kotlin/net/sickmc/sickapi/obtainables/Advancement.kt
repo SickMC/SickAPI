a3afb704fad50221742ce71fe0859268e6019a51
Anton <anton.wiblishauser@gmx.de>
2022-10-22 20:16:57 +0200

    add description to obtainables

--- src/main/kotlin/net/sickmc/sickapi/obtainables/Advancement.kt
+++ src/main/kotlin/net/sickmc/sickapi/obtainables/Advancement.kt
@@ -2,6 +2,7 @@ package net.sickmc.sickapi.obtainables
 
 import kotlinx.serialization.Serializable
 import net.sickmc.sickapi.util.ColorHolder
+import net.sickmc.sickapi.util.StaticColor
 import net.sickmc.sickapi.util.UUIDSerializer
 import java.util.*
 
@@ -10,9 +11,20 @@ data class Advancement(
     val uuid: @Serializable(with = UUIDSerializer::class) UUID,
     val uuidString: String,
     val name: String,
+    val description: String,
     val color: ColorHolder,
     val maxLevels: Int
 )
 
 @Serializable
 data class PlayerAdvancement(val advancement: Advancement, var currentLevel: Int)
+
+val Advancement.Companion.stoned: Advancement
+    get() = Advancement(
+        UUID.fromString("e8772604-ae70-4034-8fa9-616b73becee4"),
+        "e8772604-ae70-4034-8fa9-616b73becee4",
+        "stoned",
+        "Given as a reward when you reach on the SickMC Discord stone.",
+        StaticColor(0x828894),
+        1
+    )
--- src/main/kotlin/net/sickmc/sickapi/obtainables/Gadget.kt
+++ src/main/kotlin/net/sickmc/sickapi/obtainables/Gadget.kt
@@ -2,6 +2,7 @@ package net.sickmc.sickapi.obtainables
 
 import kotlinx.serialization.Serializable
 import net.sickmc.sickapi.util.ColorHolder
+import net.sickmc.sickapi.util.StaticColor
 import net.sickmc.sickapi.util.UUIDSerializer
 import java.util.*
 
@@ -10,9 +11,30 @@ data class Gadget(
     val uuid: @Serializable(with = UUIDSerializer::class) UUID,
     val uuidString: String,
     val name: String,
+    val description: String,
     val color: ColorHolder,
     val levels: Int
 )
 
 @Serializable
 data class PlayerGadget(val gadget: Gadget, var currentLevel: Int)
+
+val Gadget.Companion.gold
+    get() = Gadget(
+        UUID.fromString("d2c75c9a-d880-4bd6-9237-8d60ca134139"),
+        "d2c75c9a-d880-4bd6-9237-8d60ca134139",
+        "gold",
+        "Given as a reward when you reach gold on the SickMC Discord",
+        StaticColor(0xAA921E),
+        1
+    )
+
+val Gadget.Companion.emerald
+    get() = Gadget(
+        UUID.fromString("d7376767-6f5b-4274-9a43-6094f852fca5"),
+        "d7376767-6f5b-4274-9a43-6094f852fca5",
+        "emerald",
+        "Given as a reward when you reach emerald on the SickMC Discord",
+        StaticColor(0x127B0D),
+        1
+    )

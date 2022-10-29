865f8a74eb91e46db14462081e4a434bc8de9bf2
Anton <anton.wiblishauser@gmx.de>
2022-10-29 21:41:19 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "net.sickmc"
-version = "1.0.9"
+version = "1.0.10"
 description = "API providing fundamentals for the SickMC components"
 val isSnapshot = false
 val authors = listOf("btwonion")

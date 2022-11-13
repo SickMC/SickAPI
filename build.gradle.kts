37b0f572bd5452d6ca188fc0dcc2b069e3339e4b
Anton <anton.wiblishauser@gmx.de>
2022-11-13 20:58:22 +0100

    add datetime

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
     val ktorVersion = "2.1.3"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
